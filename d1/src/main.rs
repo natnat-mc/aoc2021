fb75820eded7b0b7b0e9f670c979fd6cdca09734
codinget <nathan.decher@gmail.com>
2021-12-02 14:42:36 +0100

    Day 1 - rust fold

--- d1/src/main.rs
+++ d1/src/main.rs
@@ -20,6 +20,17 @@ fn main() -> Result<(), Error> {
     }
 
     {
+        let (_, increases) = data
+            .iter()
+            .fold((u64::MAX, 0u64), |(last, increases), &val| if val > last {
+                (val, increases+1)
+            } else {
+                (val, increases)
+            });
+        println!("Part 1 (fold): {}", increases);
+    }
+
+    {
         let mut last = u64::MAX;
         let mut increases = 0u64;
         for val in data.windows(3).map(|x| x.iter().fold(0, |a, &b| a+b)) {
@@ -29,6 +40,18 @@ fn main() -> Result<(), Error> {
             last = val;
         }
         println!("Part 2: {}", increases);
+    }
+
+    {
+        let (_, increases) = data
+            .windows(3)
+            .map(|x| x.iter().fold(0, |a, &b| a+b))
+            .fold((u64::MAX, 0u64), |(last, increases), val| if val > last {
+                (val, increases+1)
+            } else {
+                (val, increases)
+            });
+        println!("Part 2 (fold): {}", increases);
     }
 
     Ok(())
